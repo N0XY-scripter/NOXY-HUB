9e5119e72336193b4a2cdb720546074971a532d9
N0XY-scripter <sergogar2014@gmail.com>
2026-07-09 11:01:05 +0300

    Update script source URL for NOXY-HUB

--- NOXY-HUB.lua
+++ NOXY-HUB.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/SiriusSoftwareLtd/Rayfield/main/source.lua'))() -- N0XY HUB START
+loadstring(game:HttpGet('https://pastebin.com/raw/L9uK8bS2'))()
